33230fc780e5b86fec4cd439be14d2dcca936a2f
Pappa <dr.pappa@gmail.com>
2017-10-17 16:24:22 +0100

    res.json()

--- src/police.js
+++ src/police.js
@@ -9,10 +9,8 @@ module.exports = {
     getLocalCrime(location, done) {
         let url = `${api}${street}lat=${location.latitude}lng=${location.longitude}`;
         fetch(url)
-            .then(data => {
-                // eslint-disable-next-line no-console
-                console.log(JSON.stringify(data, null, 4));
-                done(null, data);
+            .then(res => {
+                done(null, res.json());
             })
             .catch(done);
     }
