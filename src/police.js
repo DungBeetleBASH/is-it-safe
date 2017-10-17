b9029974020a7dcf2b8d182c3024c16b82bc8a22
Robert Hill <dr.pappa@gmail.com>
2017-10-17 20:21:19 +0100

    make crime response

--- src/police.js
+++ src/police.js
@@ -5,20 +5,38 @@ const fetch = require('node-fetch');
 const api = 'https://data.police.uk/api';
 const street = '/crimes-street/all-crime?';
 
+function makeResponse(crimes) {
+    let crimeIncidents = {};
+    let crimeCategories = [];
+    crimes.forEach(crime => {
+        let category = crimeIncidents[crime.category];
+        if (category !== undefined) {
+            category++;
+        } else {
+            category = 1;
+            crimeCategories.push(crime.category);
+        }
+    });
+    // eslint-disable-next-line no-console
+    console.log('crimeIncidents: ' + crimeIncidents);
+
+    return  {
+        crimeIncidents: crimeIncidents,
+        crimeCategories: crimeCategories
+    };
+}
+
 module.exports = {
     getLocalCrime(location, done) {
         let url = `${api}${street}lat=${location.latitude}&lng=${location.longitude}`;
-        // eslint-disable-next-line no-console
-        console.log('url: ' + url);
         fetch(url)
             .then(res => res.json())
             .then(json => {
-                // eslint-disable-next-line no-console
-                console.log('json: ' + typeof json);
-                done(null, json);
+                if (Array.isArray(json)) {
+                    return done(null, makeResponse(json));
+                }
+                done(null, makeResponse([]));
             })
-            .catch(e => {
-                done(e);
-            });
+            .catch(done);
     }
 };
