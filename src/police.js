f895353cf5e98a47afe120fbf692bb6de8748445
Robert Hill <dr.pappa@gmail.com>
2017-10-17 19:39:29 +0100

    url

--- src/police.js
+++ src/police.js
@@ -7,7 +7,7 @@ const street = '/crimes-street/all-crime?';
 
 module.exports = {
     getLocalCrime(location, done) {
-        let url = `${api}${street}lat=${location.latitude}lng=${location.longitude}`;
+        let url = `${api}${street}lat=${location.latitude}&lng=${location.longitude}`;
         // eslint-disable-next-line no-console
         console.log('url: ' + url);
         fetch(url)
